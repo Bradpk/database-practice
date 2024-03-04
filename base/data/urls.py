952fa116a996cc03fcfeb7bdb37dc97a40e7f929
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-03-04 14:05:43 -0500

    g

---
+++ base/data/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+from rest_framework import routers
+from .views import *
+#------------------------------------------------------------------------------------------------------------------------------
+router = routers.DefaultRouter()
+router.register(r'books', BookViewSet)
+#------------------------------------------------------------------------------------------------------------------------------
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
--- base/data/views.py
+++ base/data/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework import viewsets
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status, permissions, generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import action
+from .models import *
+from .serializers import *
+from django.db.models import Sum
 
 # Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
