f688e9ecff3350ebb8516fabbd29f3f91d20e0f2
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-03-04 14:40:16 -0500

    g

--- base/data/serializers.py
+++ base/data/serializers.py
@@ -4,4 +4,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'publication_date']
+        fields = ['id', 'title', 'author']
